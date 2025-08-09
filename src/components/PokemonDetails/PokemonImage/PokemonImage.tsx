8dc6f4577fbd9dbfccb177400f1ebc35d504b30d
Anderson Heidrich Leite <83857848+andersonheidrich@users.noreply.github.com>
2025-08-09 02:40:12 -0300

    Update PokemonImage.tsx

--- src/components/PokemonDetails/PokemonImage/PokemonImage.tsx
+++ src/components/PokemonDetails/PokemonImage/PokemonImage.tsx
@@ -1,13 +1,13 @@
-const PokemonImage = () => {
-  return (
-    <div className="flex w-[600px] justify-center items-center">
-      <img
-        className="flex w-[400px] h-[400px] bg-[#F2F2F2] rounded-[8px]"
-        src={pokemon.sprite}
-        alt={pokemon.name}
-      />
-    </div>
-  );
-};
+// const PokemonImage = () => {
+//   return (
+//     <div className="flex w-[600px] justify-center items-center">
+//       <img
+//         className="flex w-[400px] h-[400px] bg-[#F2F2F2] rounded-[8px]"
+//         src={pokemon.sprite}
+//         alt={pokemon.name}
+//       />
+//     </div>
+//   );
+// };
 
-export default PokemonImage;
+// export default PokemonImage;
