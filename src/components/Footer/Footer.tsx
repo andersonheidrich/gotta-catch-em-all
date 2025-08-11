31c1b90270b8c2987e0eed0b2eb9a278334b87da
andersonheidrich <andersonheidrich@hotmail.com>
2025-08-11 02:40:04 -0300

    feat: criado o Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,41 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col w-full p-[64px] items-center">
+      <h1>CONTATO</h1>
+      <div className="flex w-full justify-around pt-[64px] px-[64px]">
+        <div className="flex flex-col items-center w-[240px]">
+          <a
+            href="https://www.linkedin.com/in/andersonheidrich/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInIcon
+              style={{ width: "64px", height: "64px", cursor: "pointer" }}
+            />
+          </a>
+        </div>
+        <div className="flex flex-col items-center w-[240px]">
+          <a
+            href="https://github.com/andersonheidrich/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon
+              style={{ width: "64px", height: "64px", cursor: "pointer" }}
+            />
+          </a>
+        </div>
+        <div className="flex flex-col items-center w-[240px]">
+          <EmailIcon style={{ width: "64px", height: "64px" }} />
+          <span>andersonheidrichleite@gmail.com</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
+export { default as Footer } from "./Footer";
 export { default as Pokedex } from "./Pokedex";
 export { default as PokemonList } from "./PokemonList";
 export { default as Header } from "./Header";
